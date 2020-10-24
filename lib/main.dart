e43dbb1b8ac6eaf5223251b456876fefb8b95ce9
El_Giancar <giancarmine.cuofano@gmail.com>
2020-10-24 12:03:16 +0200

    Add note name

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audio_cache.dart';
 
@@ -10,12 +12,26 @@ class XylophoneApp extends StatelessWidget {
     player.play('note$sound.wav');
   }
 
-  Expanded buildKey({int sound, Color color}) {
+  Expanded buildKey({int sound, Color color, String note}) {
     return Expanded(
       child: FlatButton(
         onPressed: () {
           playSound(sound);
         },
+        child: Text(
+          note,
+          style: TextStyle(
+            fontSize: 40.0,
+            color: Colors.white,
+            shadows: [
+              Shadow(
+                blurRadius: 10.0,
+                color: Colors.black,
+                offset: Offset(5.0, 5.0),
+              ),
+            ],
+          ),
+        ),
         color: color,
       ),
     );
@@ -30,13 +46,13 @@ class XylophoneApp extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              buildKey(sound: 1, color: Colors.red),
-              buildKey(sound: 2, color: Colors.orange),
-              buildKey(sound: 3, color: Colors.yellow),
-              buildKey(sound: 4, color: Colors.green),
-              buildKey(sound: 5, color: Colors.teal),
-              buildKey(sound: 6, color: Colors.blue),
-              buildKey(sound: 7, color: Colors.purple),
+              buildKey(sound: 1, color: Colors.red, note: 'DO'),
+              buildKey(sound: 2, color: Colors.orange, note: 'RE'),
+              buildKey(sound: 3, color: Colors.yellow, note: 'MI'),
+              buildKey(sound: 4, color: Colors.green, note: 'FA'),
+              buildKey(sound: 5, color: Colors.teal, note: 'SOL'),
+              buildKey(sound: 6, color: Colors.blue, note: 'LA'),
+              buildKey(sound: 7, color: Colors.purple, note: 'SI'),
             ],
           ),
         ),
